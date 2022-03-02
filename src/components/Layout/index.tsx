fa86b7ad84705be2680d46b830e26ed5c4667f6e
leejy001 <greatness007@naver.com>
2022-03-02 20:57:51 +0900

    design fix: 반응형 수정

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -10,31 +10,36 @@ function Index({ children }: PropsWithChildren<any>) {
   const location = useLocation();
 
   return (
-    <>
+    <LayoutWrapper>
       {location.pathname === '/sign-up' || location.pathname === '/sign-in' ? (
-        <>
-          <TopWrapper>
-            <Header />
-          </TopWrapper>
-          <main>{children}</main>
-        </>
+        <TopWrapper>
+          <Header />
+        </TopWrapper>
       ) : (
-        <>
-          <TopWrapper>
-            <Header />
-            <Nav />
-          </TopWrapper>
-          <main>{children}</main>
-          <BottomWrapper>
-            <Footer />
-          </BottomWrapper>
-        </>
+        <TopWrapper>
+          <Header />
+          <Nav />
+        </TopWrapper>
       )}
-    </>
+      <main>{children}</main>
+      <BottomWrapper>
+        <Footer />
+      </BottomWrapper>
+    </LayoutWrapper>
   );
 }
 
 export default Index;
+
+const LayoutWrapper = styled.section`
+  display: flex;
+  height: 100vh;
+  flex-direction: column;
+  margin: auto;
+  main {
+    flex-grow: 1;
+  }
+`;
 
 const TopWrapper = styled.section`
   background-color: #1e90ff;
--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
 function Index() {
-  return <div>Home</div>;
+  return (
+    <div>
+      Home
+      <img
+        src="https://bookthumb-phinf.pstatic.net/cover/164/187/16418778.jpg?type=m1&udate=20210703"
+        alt="img"
+        width={200}
+      />
+    </div>
+  );
 }
 
 export default Index;
